cdc5aa04309622dfca8005f1bcb820227f3d5b34
BaptistNK <studentnfi@gmail.com>
2026-04-09 22:54:22 +0700

    +1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,8 @@ public class Enemy : MonoBehaviour
     [Header("Настройки обзора противника")]
     public Transform playerTransform;
     public float viewRadius = 5f;
+    [Range(0f, 360f)]
+    public float viewAngle = 90f; 
     public Vector3 viewCenterOffset = Vector3.zero;
     private Vector3 viewCenter;
 
@@ -30,7 +32,11 @@ public class Enemy : MonoBehaviour
 
         if (distanceToPlayer > viewRadius)
             return;
+
         Vector3 directionToPlayer = (playerTransform.position - viewCenter).normalized;
+
+        if (!IsInViewAngle(directionToPlayer))
+            return;
 
         RaycastHit hit;
         if (Physics.Raycast(viewCenter, directionToPlayer, out hit, viewRadius))
@@ -40,6 +46,15 @@ public class Enemy : MonoBehaviour
                 Debug.Log($"{gameObject.name} видит игрока!");
             }
         }
+    }
+
+    bool IsInViewAngle(Vector3 targetDirection)
+    {
+        Vector3 forward = transform.forward;
+
+        float angle = Vector3.Angle(forward, targetDirection);
+
+        return angle <= viewAngle / 2f;
     }
 
     public Vector3 GetViewCenter()
@@ -54,11 +69,23 @@ public class Enemy : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-       // Vector3 viewCenter = GetViewCenter();
-        //float viewRadius = GetViewRadius();
+        Gizmos.color = Color.green;
+        viewCenter = transform.position + viewCenterOffset;
 
         Gizmos.DrawWireSphere(viewCenter, viewRadius);
-        Gizmos.DrawLine(transform.position, transform.position + 10 * transform.forward);
+
+        DrawViewCone();
+    }
+
+    void DrawViewCone()
+    {
+
+        Gizmos.color = Color.red;
+        Vector3 leftRay = Quaternion.AngleAxis(-viewAngle / 2f, transform.up) * transform.forward * viewRadius;
+        Vector3 rightRay = Quaternion.AngleAxis(viewAngle / 2f, transform.up) * transform.forward * viewRadius;
+
+        Gizmos.DrawRay(viewCenter, leftRay);
+        Gizmos.DrawRay(viewCenter, rightRay);
+
     }
 }
